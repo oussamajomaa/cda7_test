2b7d4d531043bbb74565687a4f37ec06a8c9f566
oussamajomaa <osm70@gmx.com>
2026-07-02 11:48:20 +0200

    playwright

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,10 +1,12 @@
-const { test, expect} = require('@playwright/test')
+ 
+const { test, expect } = require("@playwright/test");
 
-test("Tester la connexion", async ({page})=>{
-    await page.goto('http://localhost:3000')
-    await page.getByLabel('Email').fill('exemple@gmail.com')
-    await page.getByLabel('Password').fill('12345678')
-    await page.getByRole('button', {name:'Login'}).click()
-    const message = page.locator('#message')
-    await expect(message).toHaveText('Connexion réussie !')
+test("Login test", async ({ page }) => {
+    await page.goto("http://localhost:3000");
+    await page.fill("#email", "machin@email.fr");
+    await page.fill("#password", "azertyTEST222!");
+    await page.click("#submit");
+    const messgae = page.locator("#message");
+    await expect(messgae).toBeVisible();
+    await expect(messgae).toHaveText("Connexion réussie !");
 })
